55f51d56eda2cbcf6527d00a62be18d1bf264fa2
Truth2004 <nhlanzekomwandla@gmail.com>
2025-05-23 12:28:43 +0200

    Added side nav

--- script.js
+++ script.js
@@ -85,3 +85,16 @@ document.addEventListener("DOMContentLoaded", () => {
 
   typeLoop();
 });
+
+const sideNav = document.getElementById("sideNav");
+const sideNavToggle = document.getElementById("sideNavToggle");
+
+sideNavToggle.addEventListener("click", () => {
+    sideNav.classList.toggle("open");
+});
+
+document.querySelectorAll("#sideNav a").forEach(link => {
+    link.addEventListener("click", () => {
+        sideNav.classList.remove("open");
+    });
+});
